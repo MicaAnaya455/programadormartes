9989cf0b9f4afcf25ada835e9ed7a3426c04d2aa
Micaela Anaya <micaelaanaya455@gmail.com>
2022-09-02 16:54:28 -0300

    Tarea M3U3

---
+++ M3U3P/routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+
+router.post('/', async (req, res, next) =>{
+  
+  //console.log(req.body) //estoy capturando datos?
+  var nombre = req.body.nombre;
+  var apellido = req.body.apellido;
+  var email = req.body.email;
+  var telefono = req.body.tel;
+  var mensaje = req.body.mensaje; 
+
+  var obj ={
+    to: 'micaelaanaya455@gmail.com',
+    subject:'Contacto desde la Web',
+    html: nombre + " " + apellido + " se contacto a traves de la web y quiere mas informacion a este correo: " + email + ".<br> Ademas hizo este comentario : " + mensaje + ".<br> Su telefono es " + telefono
+  } //cierra var obj
+
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth:{
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  })//cierra transporter
+ 
+  var info = await transporter.sendMail(obj);
+
+  res.render('index', {
+    message:'Mensaje enviado correctamente',
+  });
+
+}); //cierra peticion del post
+
+
+module.exports = router;
