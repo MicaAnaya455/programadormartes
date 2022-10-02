aeb10b0317b6557a22005c5d6c89ccdc9272b80a
Micaela Anaya <micaelaanaya455@gmail.com>
2022-10-02 15:56:37 -0300

    tarea m4u4

--- M3U3P/app.js
+++ M3U3P/app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var loginRouter = require('./routes/admin/login');
+var adminRouter = require('./routes/admin/novedades');
 
 require('dotenv').config();
 
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', adminRouter);
 
 app.use('/historia', historiaRouter);
 app.use('/raviolones', raviolonesRouter);
--- M3U3P/routes/admin/login.js
+++ M3U3P/routes/admin/login.js
@@ -1,13 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next)  {
     res.render('admin/login',{
         layout: 'admin/layout',
     });
   });
+//a traves del metodo post va a recibir la info
 
+router.post('/', async (req, res, next) =>{
+    try {
+        var usuario = req.body.usuario; //capturamos los datos
+        var password = req.body.password;
+        console.log(req.body);
+       
+        
+        var data = await usuariosModel.getUserAndPassword(usuario, password); 
 
+        if (data != undefined){ //si la info que recibi es diferente a la de undefined me va a redireccionar a la pagina admin/login
+           
+            res.redirect('/admin/novedades');
+            
+        }else{
+            res.render('admin/login', {
+                layout: 'admin/layout',
+                error: true
+            });
+        }
+    } catch (error){
+        console.log(error);
+    }
+})
 
 
 
