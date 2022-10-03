ad490caa51526afa0d203679c87053e9bf5a7d30
Micaela Anaya <micaelaanaya455@gmail.com>
2022-10-03 16:56:56 -0300

    cambios en novedades js

--- M3U3P/routes/admin/novedades.js
+++ M3U3P/routes/admin/novedades.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next){
     res.render('admin/novedades', {
         layout:'admin/layout',
-        usuario: req.session.nombre,
+        //usuario:req.session.nombre,(si pongo esta parte de codigo no me muestra novedades)
     });
 });
 
