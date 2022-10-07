1298f4ddb4d6e33d92401efecbeb48aea9355b3a
Micaela Anaya <micaelaanaya455@gmail.com>
2022-10-07 16:33:15 -0300

    CRUD PARTE 1

--- M3U3P/routes/admin/novedades.js
+++ M3U3P/routes/admin/novedades.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('./../../models/novedadesModel');
 
-router.get('/', function (req, res, next){
+
+router.get('/', async function (req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades(); //genero una variable para que almacene esta info
     res.render('admin/novedades', {
         layout:'admin/layout',
-        usuario: req.session.nombre
-        //usuario:req.session.nombre,(si pongo esta parte de codigo no me muestra novedades)
+        usuario: req.session.nombre,
+        novedades 
     });
 });
+//Para eliminar una novedad
+router.get('/eliminar/:id', async (req, res, next)=>{
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades')
+});
+
+
 
 module.exports = router;
