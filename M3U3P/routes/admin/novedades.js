6e1332b47323b66859cadb0c834be3d8c500f079
Micaela Anaya <micaelaanaya455@gmail.com>
2022-10-20 21:46:09 -0300

    TAREAM5U3

--- M3U3P/routes/admin/novedades.js
+++ M3U3P/routes/admin/novedades.js
@@ -6,7 +6,7 @@ var novedadesModel = require('./../../models/novedadesModel');
 var util = require('util');
 var cloudinary = require('cloudinary').v2;
 const uploader = util.promisify(cloudinary.uploader.upload);
-const destroy = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 
 router.get('/', async function (req, res, next) {
